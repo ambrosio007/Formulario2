97061bf336747c54d5efaa570cfb491f47be9e07
ambrosio007 <jose.a.neto40@aluno.senai.br>
2025-02-19 17:00:23 -0300

    Melhoria formulario

--- fun.php
+++ fun.php
@@ -57,9 +57,49 @@
                     echo "<p><strong>Estado :</strong>$estado</p>";
                     echo "<p><strong>CEP :</strong>$cep</p>";
                 } else {
-                    header("Location : page.html");
+                    header("Location : page.php");
                     exit();
                 }
+
+
+                ?>
+
+                <?php
+                
+                $folder = __DIR__ ."/uploads/";
+
+                if(!file_exists($folder) || !is_dir($folder)){
+                    mkdir($folder, 0755);
+                }
+                
+                var_dump([
+                    "filesize" => ini_get("upload_max_filesize"),
+                    "postsize" => ini_get("post_max_size"),
+                ]);
+
+                $getPost = filter_input(INPUT_GET, "post", FILTER_VALIDATE_BOOLEAN);
+                var_dump($_FILES);
+
+                if($_FILES && !empty($_FILES['file']['name'])){
+                    $fileUpload = $_FILES['file'];
+                    var_dump($_FILES);
+
+                    $allowebTypes = [
+                        "image/jpeg",
+                        "image/png",
+                        "application/pdf"
+                    ];
+                
+                }elseif ( $getPost){
+                    echo "<p class='trigger waring'>Não temte eviar o Examon</p>";
+                } else{
+                    if($_FILES){
+                        echo "<p class='trigger waring'>Selecione um arquivo para ser enviado!</p>";
+                    }
+                };
+                
+               
+                var_dump(scandir(__DIR__ ."/uploads"))
 
                 ?>
 
--- page.php
+++ page.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 <body>
+
     <div class="container">
         <form action="fun.php" method="post">
                 
@@ -22,35 +23,41 @@
                     <input type="text" name="email" id="id_email" placeholder="E-mail" class="inp_style">
                     <br> <br>
                     <label for="id_nome">Nome Completo:</label>
-                    <input type="text" name="nome" id="id_nome" placeholder="nome" class="inp_style">
+                    <input type="text" name="nome" id="id_nome" placeholder="nome" class="inp_style" required>
                     <br> <br>
                     <label for="id_num_tel">Número de Telefone:</label>
-                    <input type="text" name="num_tel" id="id_num_tel" placeholder="Número de Telefone" class="inp_style">
+                    <input type="text" name="num_tel" id="id_num_tel" placeholder="Número de Telefone" class="inp_style" >
                     <br> <br>
                     <label for="id_rua">Rua:</label>
-                    <input type="text" name="rua" id="id_rua" placeholder="Rua" class="inp_style">
+                    <input type="text" name="rua" id="id_rua" placeholder="Rua" class="inp_style" required>
                     <br> <br>
                     <label for="id_comp">Complemento:</label>
-                    <input type="text" name="complemento" id="id_comp" placeholder="Complemento" class="inp_style">
+                    <input type="text" name="complemento" id="id_comp" placeholder="Complemento" class="inp_style" required>
                     <br> <br>
                     <label for="id_cidade">Cidade:</label>
-                    <input type="text" name="cidade" id="id_cidade" placeholder="Cidade" class="inp_style">
+                    <input type="text" name="cidade" id="id_cidade" placeholder="Cidade" class="inp_style" required>
                     <br> <br>
                     <label for="id_estado">Estado:</label>
-                    <input type="text" name="estado" id="id_estado" placeholder="Estado" class="inp_style">
+                    <input type="text" name="estado" id="id_estado" placeholder="Estado" class="inp_style" required>
                     <br> <br>
                     <label for="id_cep">CEP:</label>
-                    <input type="text" name="cep" id="id_cep" placeholder="CEP" class="inp_style">
-                    <br><br>
-                    <input class="btn" type="submit">
-
-                </div>
-        
-                   
+                    <input type="text" name="cep" id="id_cep" placeholder="CEP" class="inp_style" required>
                 
                 </div>
+        
+                   <form name="post" action="./?post=true" method="post" enctype="multipart/form-data">
+                    <p style="margin-bottom: 10px; text-align: right"><a href="./" title="Atualizar">Atualizar</a></p>
+                    <input type="file" name="file">
+                   </form>
+                    <br> <br>
+                   <input class="btn" type="submit">
+                   
+                </div>
+
            
             </form>
     </div>        
+
+
 </body>
 </html>
